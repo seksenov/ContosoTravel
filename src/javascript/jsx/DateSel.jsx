b9310cde2640560097aa9f04a38bd3ea35405731
Alex Lu <alexlu@Alexs-rMBP.local>
2015-02-17 00:33:46 -0800

    allow for selecting two dates in calendar

--- src/javascript/jsx/DateSel.jsx
+++ src/javascript/jsx/DateSel.jsx
@@ -1,11 +1,15 @@
 var React = require('react');
 var $ = require('jquery');
 
-require('jquery-ui');
+//require('jquery-ui');
+require('multiDatesPicker');
 
 var DateSel = React.createClass({
   componentDidMount: function() {
-    $('#datepicker').datepicker();
+    //$('#datepicker').datepicker();
+    $('#datepicker').multiDatesPicker({
+      maxPicks: 2
+    });
   },
   render: function() {
     return (
