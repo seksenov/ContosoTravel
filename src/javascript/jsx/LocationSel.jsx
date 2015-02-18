6366dbba4dee80ecb8a92fe1b65853dddb9e350e
Alex Lu <alex@example.com>
2015-02-18 11:26:58 -0800

    added refresh button

--- src/javascript/jsx/LocationSel.jsx
+++ src/javascript/jsx/LocationSel.jsx
@@ -4,15 +4,19 @@ var LocationSel = React.createClass({
   handleClick: function(i) {
     this.props.onChosenCity(i);
   },
+  handleRefresh: function() {
+    window.location.reload(true);
+  },
   render: function() {
     return (
       <div id="destContainer">
-      {this.props.cities.map(function(city, i) {
-        return (
-          <div className={"dest " + city} onClick={this.handleClick.bind(this, i)} key={i}><div className="title">{city}</div></div>
-        );
-       }, this)}
-      <div id="locationPrompt">Where do you want to go?</div>
+        <div onClick={this.handleRefresh} id="refreshButton">Refresh</div>
+        {this.props.cities.map(function(city, i) {
+          return (
+            <div className={"dest " + city} onClick={this.handleClick.bind(this, i)} key={i}><div className="title">{city}</div></div>
+          );
+         }, this)}
+        <div id="locationPrompt">Where do you want to go?</div>
       </div>
     );
   }
