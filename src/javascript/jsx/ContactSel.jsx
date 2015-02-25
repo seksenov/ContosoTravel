5e6ad82214450b3eeb07514732f24ff3ec5821a1
Melanie Richards <mr@melanie-richards.com>
2015-02-25 15:59:30 -0800

    Restructured friend pane markup for new style

--- src/javascript/jsx/ContactSel.jsx
+++ src/javascript/jsx/ContactSel.jsx
@@ -8,11 +8,18 @@ var ContactSel = React.createClass({
   render: function() {
     return (
       <div>
-        <div className="contentContainer">
-          <div className="promptText">Who do you want to go with?</div>
-          <button className="largeButton action-button button-animate blue" onClick={this.handleClick}>Choose a Friend</button>
-          <div id="contactName">{this.props.contacts}</div>
-          <button className="confirmButton action-button button-animate blue" onClick={this.handleConfirm}>Confirm</button>
+        <div className="container paneContainer">
+          <span className="stepLabel">Step 3 of 4</span>
+          <h2 className="paneTitle">Who do you want to go with?</h2>
+          <div className="paneContent">
+            <button className="buttonAction" onClick={this.handleClick}>Choose a Friend</button>
+            <div id="contactName">{this.props.contacts}</div>
+          </div>
+        </div>
+        <div className="paneButtons">
+        	<div className="container">
+	        	<button className="buttonAction buttonConfirm" onClick={this.handleConfirm}>Confirm</button>
+	        </div>
         </div>
         <div className="Friends background"></div>
       </div>
