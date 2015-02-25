89e58a1ed29fdd06f9c06ac90f8f74f537c6479b
Alex Lu <alex@example.com>
2015-02-25 11:37:12 -0800

    Changed date selection page to match Melanie's markup

--- src/javascript/jsx/DateSel.jsx
+++ src/javascript/jsx/DateSel.jsx
@@ -44,15 +44,18 @@ var DateSel = React.createClass({
   render: function() {
     return (
       <div>
-        <div id="calendarContainer">
-          <div id="textDateContainer">
-            <div className="promptText">When do you want to go?</div>
+      <div className="paneContainer">
+        <span className="stepLabel">Step # of 5</span>
+        <h2 className="paneTitle">When do you want to go?</h2>
+        <div className="paneContent">
             <div className="dateText" id="departure" onClick={this.handleClick.bind(this, 'departure')}>Departure: <span className="dateClickable">{this.state.departure}</span></div> 
             <div className="dateText" id="returning" onClick={this.handleClick.bind(this, 'returning')}>Return: <span className="dateClickable">{this.state.returning}</span></div> 
             <button className="confirmButton action-button button-animate blue" onClick={this.handleConfirm}>Confirm</button>
-          </div>
+        </div>
+        <div id="calendarContainer">
           <div id="datepicker" className="calendar"></div>
         </div>
+      </div>
         <div className ={this.props.currentCity + "Second background"}></div>
       </div>
     );
