ca86a8287c6de008c31c6968a4b65065675e568f
Melanie Richards <mr@melanie-richards.com>
2015-02-25 19:01:27 -0800

    Restructured location selection for new styling
    This version shows white buttons over the images.

--- src/javascript/jsx/LocationSel.jsx
+++ src/javascript/jsx/LocationSel.jsx
@@ -9,14 +9,27 @@ var LocationSel = React.createClass({
   },
   render: function() {
     return (
-      <div id="destContainer">
-        {this.props.cities.map(function(city, i) {
-          return (
-            <div className={"dest " + city} onClick={this.handleClick.bind(this, i)} key={i}><div className="title">{city}</div></div>
-          );
-         }, this)}
-        <div id="locationPrompt">Where do you want to go?</div>
+      
+      <div>
+        <div className="container paneContainer">
+          <span className="stepLabel">Step 1 of 4</span>
+          <h2 className="paneTitle">Where do you want to go?</h2>
+          <div className="paneContent">
+            <div id="destContainer">
+              {this.props.cities.map(function(city, i) {
+              return (
+                <div className={"dest " + city} onClick={this.handleClick.bind(this, i)} key={i}>
+                  <div className="destTitle">
+                    <span className="buttonAction">{city}</span>
+                  </div>
+                </div>
+              );
+              }, this)}
+            </div>
+          </div>
+        </div>
       </div>
+    
     );
   }
 });
