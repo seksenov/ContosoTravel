aa2e975c4fec8fa9b5f166614a00aee3bd485710
U-REDMOND\alelu <alelu@WIN-AL.redmond.corp.microsoft.com>
2015-05-13 16:51:05 -0700

    added web.config into build system

--- gulpfile.js/tasks/production.js
+++ gulpfile.js/tasks/production.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
 
 // Run this to compress all the things!
-gulp.task('production', ['minifyImg', 'minifyCss', 'uglifyJs']);
+gulp.task('production', ['minifyImg', 'minifyCss', 'uglifyJs'], function() {
+  return gulp.src('./src/web.config')
+  .pipe(gulp.dest('./build'));
+});
