9743e1a5259e045cb11a4cd27cddd78eac45bf2f
Kiril Seksenov <seksenov@outlook.com>
2015-06-23 15:37:39 -0700

    adding comments

--- src/javascript/vendor/plugins.js
+++ src/javascript/vendor/plugins.js
@@ -1,7 +1,7 @@
 var moment = require('moment');
 
 exports.addAppointment = function(cb, date, loc) {
-  if (typeof Windows != 'undefined') {
+  if (typeof Windows !== 'undefined') {
     var appointment = new Windows.ApplicationModel.Appointments.Appointment();
 
     appointment.allDay = true;
@@ -23,7 +23,7 @@ exports.addAppointment = function(cb, date, loc) {
 }
 exports.addContact = function(cb) {
   console.log('plugin: addContact');
-  if(typeof Windows != 'undefined') {
+  if(typeof Windows !== 'undefined') {
     // Create the picker 
     var picker = new Windows.ApplicationModel.Contacts.ContactPicker(); 
     picker.desiredFieldsWithContactFieldType.append(Windows.ApplicationModel.Contacts.ContactFieldType.email);
@@ -31,14 +31,9 @@ exports.addContact = function(cb) {
     picker.pickContactAsync().done(function (contact) { 
       if (contact !== null) { 
         var name = contact.displayName; 
-        console.log(name);
-        //var thumbnail = URL.createObjectURL(contact.thumbnail, { oneTimeOnly: true }); 
-        //var picture = document.createElement("image");
-        //picture.src = thumbnail;
         cb({'name': name}, null);
       } else { 
         // The picker was dismissed without selecting a contact 
-        console.log("No contact was selected");
         cb(null, "No contact was selected"); 
       } 
       
@@ -53,34 +48,34 @@ exports.showToast = function(message) {
   // Log the message to the console
   console.log("OUTPUT: " + message);
 
-  if(typeof Windows != 'undefined') {
-    // Log to the console
-    var notifications = Windows.UI.Notifications;
-    //Get the XML template where the notification content will be suplied
-    var template = notifications.ToastTemplateType.toastImageAndText01;
-    var toastXml = notifications.ToastNotificationManager.getTemplateContent(template);
-    //Supply the text to the XML content
-    var toastTextElements = toastXml.getElementsByTagName("text");
-    toastTextElements[0].appendChild(toastXml.createTextNode(message));
-    //Supply an image for the notification
-    var toastImageElements = toastXml.getElementsByTagName("image");
-    //Set the image this could be the background of the note, get the image from the web
-    toastImageElements[0].setAttribute("src", "https://raw.githubusercontent.com/seksenov/ContosoTravel/master/build/images/storelogo.png");
-    toastImageElements[0].setAttribute("alt", "red graphic");
-    //Specify a long duration
-    var toastNode = toastXml.selectSingleNode("/toast");
-    toastNode.setAttribute("duration", "long");
-    //Specify the audio for the toast notification
-    var toastNode = toastXml.selectSingleNode("/toast");                        
-    var audio = toastXml.createElement("audio");
-    audio.setAttribute("src", "ms-winsoundevent:Notification.IM");
-    //Specify launch paramater
-    toastXml.selectSingleNode("/toast").setAttribute("launch", '{"type":"toast","param1":"12345","param2":"67890"}');
-    //Create a toast notification based on the specified XML
-    var toast = new notifications.ToastNotification(toastXml);
-    //Send the toast notification
-    var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
-    toastNotifier.show(toast);
+  // if(typeof Windows !== 'undefined') {
+  //   // Log to the console
+  //   var notifications = Windows.UI.Notifications;
+  //   //Get the XML template where the notification content will be suplied
+  //   var template = notifications.ToastTemplateType.toastImageAndText01;
+  //   var toastXml = notifications.ToastNotificationManager.getTemplateContent(template);
+  //   //Supply the text to the XML content
+  //   var toastTextElements = toastXml.getElementsByTagName("text");
+  //   toastTextElements[0].appendChild(toastXml.createTextNode(message));
+  //   //Supply an image for the notification
+  //   var toastImageElements = toastXml.getElementsByTagName("image");
+  //   //Set the image this could be the background of the note, get the image from the web
+  //   toastImageElements[0].setAttribute("src", "https://raw.githubusercontent.com/seksenov/ContosoTravel/master/build/images/storelogo.png");
+  //   toastImageElements[0].setAttribute("alt", "red graphic");
+  //   //Specify a long duration
+  //   var toastNode = toastXml.selectSingleNode("/toast");
+  //   toastNode.setAttribute("duration", "long");
+  //   //Specify the audio for the toast notification
+  //   var toastNode = toastXml.selectSingleNode("/toast");                        
+  //   var audio = toastXml.createElement("audio");
+  //   audio.setAttribute("src", "ms-winsoundevent:Notification.IM");
+  //   //Specify launch paramater
+  //   toastXml.selectSingleNode("/toast").setAttribute("launch", '{"type":"toast","param1":"12345","param2":"67890"}');
+  //   //Create a toast notification based on the specified XML
+  //   var toast = new notifications.ToastNotification(toastXml);
+  //   //Send the toast notification
+  //   var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
+  //   toastNotifier.show(toast);
 
   } else {
     //TODO: Fallback to website functionality
