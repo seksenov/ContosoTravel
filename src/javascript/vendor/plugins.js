6113f260c1e4c10c7d4e67a1864df90be848689e
Kiril Seksenov <seksenov@outlook.com>
2015-06-23 15:41:25 -0700

    removing notifications

--- src/javascript/vendor/plugins.js
+++ src/javascript/vendor/plugins.js
@@ -48,34 +48,34 @@ exports.showToast = function(message) {
   // Log the message to the console
   console.log("OUTPUT: " + message);
 
-  if(typeof Windows !== 'undefined') {
-    // Log to the console
-    var notifications = Windows.UI.Notifications;
-    //Get the XML template where the notification content will be suplied
-    var template = notifications.ToastTemplateType.toastImageAndText01;
-    var toastXml = notifications.ToastNotificationManager.getTemplateContent(template);
-    //Supply the text to the XML content
-    var toastTextElements = toastXml.getElementsByTagName("text");
-    toastTextElements[0].appendChild(toastXml.createTextNode(message));
-    //Supply an image for the notification
-    var toastImageElements = toastXml.getElementsByTagName("image");
-    //Set the image this could be the background of the note, get the image from the web
-    toastImageElements[0].setAttribute("src", "https://raw.githubusercontent.com/seksenov/ContosoTravel/master/build/images/storelogo.png");
-    toastImageElements[0].setAttribute("alt", "red graphic");
-    //Specify a long duration
-    var toastNode = toastXml.selectSingleNode("/toast");
-    toastNode.setAttribute("duration", "long");
-    //Specify the audio for the toast notification
-    var toastNode = toastXml.selectSingleNode("/toast");                        
-    var audio = toastXml.createElement("audio");
-    audio.setAttribute("src", "ms-winsoundevent:Notification.IM");
-    //Specify launch paramater
-    toastXml.selectSingleNode("/toast").setAttribute("launch", '{"type":"toast","param1":"12345","param2":"67890"}');
-    //Create a toast notification based on the specified XML
-    var toast = new notifications.ToastNotification(toastXml);
-    //Send the toast notification
-    var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
-    toastNotifier.show(toast);
+  // if(typeof Windows !== 'undefined') {
+  //   // Log to the console
+  //   var notifications = Windows.UI.Notifications;
+  //   //Get the XML template where the notification content will be suplied
+  //   var template = notifications.ToastTemplateType.toastImageAndText01;
+  //   var toastXml = notifications.ToastNotificationManager.getTemplateContent(template);
+  //   //Supply the text to the XML content
+  //   var toastTextElements = toastXml.getElementsByTagName("text");
+  //   toastTextElements[0].appendChild(toastXml.createTextNode(message));
+  //   //Supply an image for the notification
+  //   var toastImageElements = toastXml.getElementsByTagName("image");
+  //   //Set the image this could be the background of the note, get the image from the web
+  //   toastImageElements[0].setAttribute("src", "https://raw.githubusercontent.com/seksenov/ContosoTravel/master/build/images/storelogo.png");
+  //   toastImageElements[0].setAttribute("alt", "red graphic");
+  //   //Specify a long duration
+  //   var toastNode = toastXml.selectSingleNode("/toast");
+  //   toastNode.setAttribute("duration", "long");
+  //   //Specify the audio for the toast notification
+  //   var toastNode = toastXml.selectSingleNode("/toast");                        
+  //   var audio = toastXml.createElement("audio");
+  //   audio.setAttribute("src", "ms-winsoundevent:Notification.IM");
+  //   //Specify launch paramater
+  //   toastXml.selectSingleNode("/toast").setAttribute("launch", '{"type":"toast","param1":"12345","param2":"67890"}');
+  //   //Create a toast notification based on the specified XML
+  //   var toast = new notifications.ToastNotification(toastXml);
+  //   //Send the toast notification
+  //   var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
+  //   toastNotifier.show(toast);
 
   } else {
     //TODO: Fallback to website functionality
